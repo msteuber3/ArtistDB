ba667a01c3c5136fe492d68a0e62b24a3294a679
theamazingmeep <michaelthesteuber@gmail.com>
2025-02-05 19:58:52 -0500

    fixed cors issue

--- database.py
+++ database.py
@@ -4,7 +4,11 @@ from dataclasses import dataclass
 import datetime
 from pathlib import Path
 from Entry import *
+from flask import Flask
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app)
 
 def Setup():
     conn = sqlite3.connect('artists.db')
@@ -33,6 +37,15 @@ def remove_entry(db, entry: Entry):
     cur.execute(entry.to_sql_delete_query())
     db.commit()
 
+@app.route('/getTable', methods=['GET'])
+def get_html_table():
+    conn = sqlite3.connect()
+    df = pd.read_sql("SELECT * FROM artists", conn)
+    htmlTable = df.to_html()
+    response = Flask.jsonify({'data' : htmlTable})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
 
 def main():
     dbFile = Path('artists.db')
@@ -42,4 +55,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    app.run()
